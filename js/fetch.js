9db12b9a52b91e96f6ef03c7230a822d340a9d91
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-03-05 13:14:15 +0600

    get data from json placeholder and dynamically added

--- js/fetch.js
+++ js/fetch.js
@@ -1,8 +1,40 @@
 function loadData(){
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(data => showData(data))
 }
-function showData(json){
-    console.log(json);
+function showData(users){
+    // console.log(users);
+    const userData =document.getElementById('user-list');
+
+    for(const user of users){
+        // console.log(user);
+        const list=document.createElement('li');
+        
+        list.innerText= user.name;
+        userData.appendChild(list);
+
+
+    }
 }
+function postsData(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(json => showPosts(json))
+}
+function showPosts(data){
+    const userPosts =document.getElementById('posts-div');
+    for(const user of data){
+        // console.log(user);
+        const divPost=document.createElement('div')
+        divPost.classList.add('posts')
+        divPost.innerHTML=`
+                        <h4>User ID:${user.id}</h4>
+                        <h4>User Title${user.title} </h4>
+                        <p> User Description${user.body}</p>
+        `
+       
+        userPosts.appendChild(divPost);
+    }
+}
+postsData();
